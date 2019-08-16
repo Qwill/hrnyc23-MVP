7f9a9825b8ab23663023d78eed107bb3990adb5b
Nick Pence <pencenj@gmail.com>
2019-08-16 12:50:33 -0400

    add some formatting

--- app.jsx
+++ app.jsx
@@ -4,61 +4,62 @@ import axios from 'axios'
 import parser from 'react-html-parser'
 
 class App extends React.Component {
-    constructor () {
+    constructor() {
         super()
         this.state = {
-           input: '',
-           output: []
+            input: '',
+            output: []
         }
         this.changeInput = this.changeInput.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
     changeInput(e) {
-        this.setState({input: e.target.value})
+        this.setState({ input: e.target.value })
     }
     handleSubmit(e) {
         e.preventDefault()
         axios.get(`/scrape?url=${this.state.input}`)
-        .then(({data}) => {
-            console.log(data)
-            let arr = []
-            for (let key in data) {
-                let obj = {}
-                obj.id = key
-                obj.date = data[key].date
-                obj.text = data[key].text
-                arr.push(obj)
-            }
-            const compare = (a, b) => {
-                if (Number(a.id) > Number(b.id)) {
-                    return -1
+            .then(({ data }) => {
+                console.log(data)
+                let arr = []
+                let name = this.state.input
+                for (let key in data) {
+                    let obj = {}
+                    obj.id = `https://twitter.com/${name}/status/${key}`
+                    obj.date = data[key].date
+                    obj.text = data[key].text
+                    arr.push(obj)
                 }
-                if (Number(a.id) < Number(b.id)) {
-                    return 1
+                const compare = (a, b) => {
+                    if (Number(a.id) > Number(b.id)) {
+                        return -1
+                    }
+                    if (Number(a.id) < Number(b.id)) {
+                        return 1
+                    }
+                    return 0
                 }
-                return 0
-            }
-            arr.sort(compare)
-            this.setState({output: arr})
-        })
-        .catch((err) => {
-            console.log('Error: ', err)
-        })
+                arr.sort(compare)
+                this.setState({ output: arr })
+            })
+            .catch((err) => {
+                console.log('Error: ', err)
+            })
     }
-    render () {
+    render() {
         return (
             <React.Fragment>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.input} onChange={this.changeInput}></input>
+                    <input type="text" placeholder="Enter Twitter handle here" value={this.state.input} onChange={this.changeInput}></input>
                     <input type="submit" value="Submit" />
                 </form>
                 {this.state.output.map((tweet, idx) => {
                     return (
                         <React.Fragment key={idx}>
-                        <p>{tweet.id}</p>
-                        <p>{tweet.date}</p>
-                        <p>{parser(tweet.text)}</p>
-                        <hr/>
+                            <a href={tweet.id}>{tweet.id}</a>
+                            <p style={{ float: right }}>{tweet.date}</p>
+                            <p>{parser(tweet.text)}</p>
+                            <hr />
                         </React.Fragment>
                     )
                 })}
