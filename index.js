e9959f09504abcbdc752eb2271b5d328dc0739bd
Ubuntu <ubuntu@ip-172-31-35-102.us-east-2.compute.internal>
2019-08-17 20:58:42 +0000

    functional scroll and extraction! success!

--- index.js
+++ index.js
@@ -13,32 +13,30 @@ app.use(cors())
 app.get('/scrape', async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setJavaScriptEnabled(true)
     await page.goto(`https://twitter.com/${req.query.url}`);
-    await page.setJavaScriptEnabled(true)
     let obj = await autoScroll(page)
     async function autoScroll(page) {
         return page.evaluate(async () => {
             return await new Promise((resolve, reject) => {
                 let obj = {}
-                var totalHeight = 0;
-                var distance = 100;
-                var count = 0
-                var timer = setInterval(() => {
-                    var scrollHeight = document.body.scrollHeight;
+                let totalHeight = 0;
+                let distance = 400;
+                let count = 0
+                const timer = setInterval(() => {
+                    let scrollHeight = document.body.scrollHeight;
                     window.scrollBy(0, distance);
                     totalHeight += distance;
-                    try {
                     Array.apply(null, document.getElementsByClassName('js-stream-item')).forEach((tweet) => {
-                      let id = tweet.getAttribute('data-item-id')
-                      let date = tweet.childNodes[1].childNodes[3].childNodes[1].childNodes[3].childNodes[1].getAttribute('title')
-                      let text = tweet.childNodes[1].childNodes[3].childNodes[3].childNodes[1].innerHTML
-                      obj[id] = { date: date, text: text }
+                      try {
+                        let id = tweet.getAttribute('data-item-id')
+                        let date = tweet.childNodes[1].childNodes[3].childNodes[1].childNodes[3].childNodes[1].getAttribute('title')
+                        let text = tweet.childNodes[1].childNodes[3].childNodes[3].childNodes[1].innerHTML
+                        obj[id] = { date: date, text: text }
+                      } catch (err) {return}
                     })
-                    } catch (err) {obj.error = err.toString()}
                     count++
                     //if (totalHeight >= scrollHeight) {
-                    if (count === 20) {
+                    if (count === 100) {
                         clearInterval(timer);
                         resolve(obj);
                     }
